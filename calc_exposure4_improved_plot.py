325757e0e64d8f2239f0f49d969a897d4e1ddae7
J-Hopf <118164964+J-Hopf@users.noreply.github.com>
2023-09-11 10:31:46 +0200

    Update calc_exposure4_improved_plot.py

--- calc_exposure4_improved_plot.py
+++ calc_exposure4_improved_plot.py
@@ -12,24 +12,21 @@ import os
 from shapely.geometry import Point
 from matplotlib import pyplot as plt
 import osmnx as ox
-import random
 
-
-random.seed(10)
 os.chdir("core")
 
 filedir = "C:/Users/Jan/PycharmProjects/Thesis2/"
-results_dir = f"{filedir}results1/r1_improved/"  # use same folder as streamline_osmnx
-savedir = f"{results_dir}Uni/"  # each profile a savedir.
+results_dir = f"{filedir}results3/r3_improved/"  # Use same folder as streamline_osmnx and calc_exposure4_improved
+savedir = f"{results_dir}Uni/"
 
-# plot activity exposure
-def plotact2(sub1, sub2, act, savename="1", select_start=1, save=False):
+# Function for plotting activity exposure
+def plotact2(rows, cols, act, savename="1", select_start=1, save=False):
     schedir = savedir + 'gensche/'
 
-    fig, ax = plt.subplots(sub1, sub2, figsize=(10, 8),  # (20, 5)
+    fig, ax = plt.subplots(rows, cols, figsize=(10, 8),  # (20, 5)
                            sharey=True)
     axs = ax.flatten()
-    for i1 in range(sub1 * sub2):
+    for i1 in range(rows * cols):
         id1 = i1 + select_start
         sch = pd.read_csv(f'{schedir}ws_iter_{iteration}_id_{id1}.csv')
         st = sch['start_time']
@@ -57,34 +54,45 @@ def plotact2(sub1, sub2, act, savename="1", select_start=1, save=False):
     if save:
         fig.savefig(f'{savedir}{savename}.png')
 
-# still doing hourly
+# Function for plotting average exposure map of home locations
+def plot_exposure_map(savedir1, ite, save_gpkg=False):
+    ODdir = savedir1 + "genloc/"
+    ODfile = f'h2w_{iteration}.csv'
+    homework = gpd.read_file(ODdir + ODfile)
+
+    lat = np.array(homework.home_lat).astype(float)
+    lon = np.array(homework.home_lon).astype(float)
+
+    person = pd.read_csv(f"{savedir1}exposure/iter_{ite}_person.csv").iloc[:, 1]
+
+    df2 = pd.DataFrame({"personal_exposure": person, "lat": lat, "lon": lon})
+    exp_gdf = gpd.GeoDataFrame(df2["personal_exposure"], crs=4326, geometry=[Point(xy) for xy in zip(df2.lon, df2.lat)])
+
+    if save_gpkg:
+        exp_gdf.to_file(f'{savedir1}person_iter{ite}.gpkg')
+
+    # visualise
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    utrecht = ox.geocode_to_gdf('Utrecht')
+    utrecht.plot(ax=ax, color="gray", alpha=0.5)
+    ax.set_ylabel("Latitude")
+    ax.set_xlabel("Longitude")
+    exp_gdf.plot(ax=ax, column='personal_exposure', legend=True)
+
+
+# Specify which agents are plotted
 iteration = 31
 start_number = 20
-ODdir = savedir + "genloc/"
-ODfile = f'h2w_{iteration}.csv'
-homework = gpd.read_file(ODdir + ODfile)  # for comparison #gpd can read csv as well, just geom as None.
 
-lat = np.array(homework.home_lat).astype(float)
-lon = np.array(homework.home_lon).astype(float)
-
+# Exposure per activity
 act = pd.read_csv(f"{savedir}exposure/iter_{iteration}_act.csv")  # Because every row is a list we don't need .iloc
-person = pd.read_csv(f"{savedir}exposure/iter_{iteration}_person.csv").iloc[:, 1]
 
-df2 = pd.DataFrame({"personal_exposure": person, "lat": lat, "lon": lon})
-exp_gdf = gpd.GeoDataFrame(df2["personal_exposure"], crs=4326, geometry=[Point(xy) for xy in zip(df2.lon, df2.lat)])
-exp_gdf.to_file(f'{savedir}person_iter{iteration}.gpkg')
+# Plot activity exposure
+plotact2(rows=2, cols=1, act=act, savename="more", select_start=start_number, save=False)
 
-# visualise
-fig, ax = plt.subplots()
-ax.set_aspect('equal')
-utrecht = ox.geocode_to_gdf('Utrecht')
-utrecht.plot(ax=ax, color="gray", alpha=0.5)
-ax.set_ylabel("Latitude")
-ax.set_xlabel("Longitude")
-exp_gdf.plot(ax=ax, column='personal_exposure', legend=True)
-
-# plot activity schedule
-plotact2(sub1=2, sub2=2, act=act, savename="more", select_start=start_number, save=False)
+# Plot average exposure map of home locations
+plot_exposure_map(savedir, ite=iteration)
 
 plt.show()
 plt.close('all')
