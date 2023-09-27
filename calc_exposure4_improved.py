5945e043744b30280f6ba30efda8b3be29d0ddeb
J-Hopf <118164964+J-Hopf@users.noreply.github.com>
2023-09-27 10:41:52 +0200

    Update calc_exposure4_improved.py

--- calc_exposure4_improved.py
+++ calc_exposure4_improved.py
@@ -343,35 +343,16 @@ print(f"It took {(end - start)/60:.1f} minutes to complete 1/2.\n")
 ####################################################################################################
 # plotting...
 
-
-# still doing hourly
-ext = 0.001  # 300 m
+# Specify which agents are plotted
 iteration = 31
-ODdir = savedir + "genloc/"
-ODfile = f'h2w_{iteration}.csv'
-homework = gpd.read_file(ODdir + ODfile)  # for comparison #gpd can read csv as well, just geom as None.
+start_number = 20
 
-lat = np.array(homework.home_lat).astype(float)
-lon = np.array(homework.home_lon).astype(float)
-
+# Exposure per activity
 act = pd.read_csv(f"{savedir}exposure/iter_{iteration}_act.csv")  # Because every row is a list we don't need .iloc
-person = pd.read_csv(f"{savedir}exposure/iter_{iteration}_person.csv").iloc[:, 1]
-
-df2 = pd.DataFrame({"personal_exposure": person, "lat": lat, "lon": lon})
-exp_gdf = gpd.GeoDataFrame(df2["personal_exposure"], crs=4326, geometry=[Point(xy) for xy in zip(df2.lon, df2.lat)])
-exp_gdf.to_file(f'{savedir}person_iter{iteration}.gpkg')
-
-# visualise
-fig, ax = plt.subplots()
-ax.set_aspect('equal')
-utrecht = ox.geocode_to_gdf('Utrecht')
-utrecht.plot(ax=ax, color="gray", alpha=0.5)
-ax.set_ylabel("Latitude")
-ax.set_xlabel("Longitude")
-exp_gdf.plot(ax=ax, column='personal_exposure', legend=True)
 
 # plot activity
 plotact2(sub1=2, sub2=2, act=act, savename="more", select_start=20, save=False)
+
 plt.show()
 plt.close('all')
 
